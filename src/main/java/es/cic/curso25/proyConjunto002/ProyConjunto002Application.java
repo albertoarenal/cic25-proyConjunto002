9cfbb8a86c2546598378d017a3c6dbd2e30808be
noname <noname>
2025-07-21 12:54:59 +0200

    primer commit, proyecto base

---
+++ src/main/java/es/cic/curso25/proyConjunto002/ProyConjunto002Application.java
@@ -0,0 +1,13 @@
+package es.cic.curso25.proyConjunto002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyConjunto002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyConjunto002Application.class, args);
+	}
+
+}
