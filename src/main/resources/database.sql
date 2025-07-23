e300f7b518c33ebd3167155fea3c8849d1dbcd38
yara-deoliveira <yaradeoli@gmail.com>
2025-07-23 23:02:35 +0200

    Application properties y fichero database actualizado

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,6 +1,10 @@
 drop table if exists barco cascade
+drop table if exists capitan cascade
 drop sequence if exists barco_seq
+drop sequence if exists capitan_seq
 create sequence barco_seq start with 1 increment by 50
+create sequence capitan_seq start with 1 increment by 50
+
     create table barco (
         anio integer,
         capacidad integer,
@@ -10,3 +14,18 @@ create sequence barco_seq start with 1 increment by 50
         tipo_barco varchar(255),
         primary key (id)
     )
+
+    create table capitan (
+        barco_id bigint unique,
+        id bigint not null,
+        numero_de_licencia varchar(8) unique,
+        version bigint not null,
+        apellidos varchar(255),
+        nombre varchar(255),
+        primary key (id)
+    )
+
+    alter table if exists capitan
+       add constraint FKqrci20v6hdcy7m1vmyymrsenl
+       foreign key (barco_id)
+       references barco
